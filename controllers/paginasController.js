abe5ddeb5c9e91293b377b4b01664aade256a89c
Neri Espinola <neri.agustin.es@outlook.com>
2022-11-13 22:09:21 -0300

    feat: maqueta y estilo de la pagina de Inicio

--- controllers/paginasController.js
+++ controllers/paginasController.js
@@ -1,10 +1,23 @@
 import { Viaje } from '../models/Viaje.js';
 import { Testimonial } from '../models/Testimonial.js';
 
-const paginaInicio = (req, res) => {
-    res.render('inicio', {
-        pagina: 'Inicio'
-    });
+const paginaInicio = async (req, res) => {
+    try {
+        const viajes = Viaje.findAll({ limit: 3 });
+        const testimoniales = Testimonial.findAll({ limit: 3 });
+
+        const resultado = await Promise.all([viajes, testimoniales])
+
+        res.render('inicio', {
+            pagina: 'Inicio',
+            clase: 'home',
+            viajes: resultado[0],
+            testimoniales: resultado[1]
+        });
+        
+    } catch (error) {
+        console.log(error);   
+    }
 }
 
 const paginaNosotros = (req, res) => {
